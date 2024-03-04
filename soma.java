0c7352ce0cfddff21519f3be3a012f7f74b490fa
Thiago Benini <139355804+ThBenini@users.noreply.github.com>
2024-03-04 15:11:55 -0300

    Add files via upload

---
+++ soma.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+    public class soma {
+        public static void main(String[] args) {
+
+        }
+
+        Scanner ler = new Scanner (System.in);
+    int num1,num2,soma;
+    System.out.print("digite um valor: ");
+    num1 = ler.nextInt();
+    System.out.print("digite mais um valor: ");
+    num2 = ler.nextInt();
+
+    num1 + num2 = soma;
+
+    System.out.print("Resultado: "+ soma);
+
+}
+
